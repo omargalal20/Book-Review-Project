37ef9ae616557fd044e5b9aaa07b0a398d11b344
Omar ElHanafy <oelhanafy12@gmail.com>
2021-01-14 18:53:12 +0200

    Finalized project

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 const { futimes } = require('fs');
 var fs = require('fs');
 var path = require('path');
+var session = require('express-session');
+const bodyParser = require('body-parser');
+const { Recoverable } = require('repl');
+const router = express.Router();
 var app = express();
 
 // view engine setup
@@ -13,12 +17,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); //Setting where is the folder for images, videos, css files
 
+var sess = {
+  secret: 'balabizo',
+  saveUninitialized: false,
+  resave: false,
+  cookie: {}
+};
+app.use(session(sess));
+
 app.get('/',function(req,res){
   res.render('login', {error: ""});
 })
 
 app.get('/dune',function(req,res){
-  res.render('dune');
+  res.render('dune', {error: ""});
 })
 
 app.get('/fiction',function(req,res){
@@ -26,11 +38,11 @@ app.get('/fiction',function(req,res){
 })
 
 app.get('/flies',function(req,res){
-  res.render('flies');
+  res.render('flies', {error: ""});
 })
 
 app.get('/grapes',function(req,res){
-  res.render('grapes');
+  res.render('grapes', {error: ""});
 })
 
 app.get('/home',function(req,res){
@@ -38,11 +50,11 @@ app.get('/home',function(req,res){
 })
 
 app.get('/leaves',function(req,res){
-  res.render('leaves');
+  res.render('leaves', {error: ""});
 })
 
 app.get('/mockingbird',function(req,res){
-  res.render('mockingbird');
+  res.render('mockingbird', {error: ""});
 })
 
 app.get('/novel',function(req,res){
@@ -53,9 +65,7 @@ app.get('/poetry',function(req,res){
   res.render('poetry');
 })
 
-app.get('/readlist',function(req,res){
-  res.render('readlist');
-})
+
 
 app.get('/registration',function(req,res){
   res.render('registration', {error: ""});
@@ -66,7 +76,7 @@ app.get('/searchresults',function(req,res){
 })
 
 app.get('/sun',function(req,res){
-  res.render('sun');
+  res.render('sun', {error: ""});
 })
 
 app.post('/',function(req,res){
@@ -76,7 +86,8 @@ app.post('/',function(req,res){
   current_users = JSON.parse(current_users); 
   for (saved_user in current_users){
     if(user.localeCompare(current_users[saved_user].username) === 0 && pass.localeCompare(current_users[saved_user].password) === 0){
-      res.redirect('/home');
+      req.session.user=user;
+      return res.redirect('/home');
     }
     
   }
@@ -100,6 +111,7 @@ app.post('/register', function(req, res){
   var new_user = {
     "username": user,
     "password": pass,
+    "readList": [],
   } 
   current_users.push(new_user); // [new_user]
   var added_users = JSON.stringify(current_users)
@@ -107,6 +119,64 @@ app.post('/register', function(req, res){
   res.redirect('/');
   }
 })
+
+app.post('/addtoreadlist', function(req, res){
+  title = req.body.title;
+  file = req.body.file;
+  to_render = file + ".ejs";
+  user = req.session.user;
+  var current_users = fs.readFileSync('users.json');
+  current_users = JSON.parse(current_users); 
+  for (saved_user in current_users){
+    if(user.localeCompare(current_users[saved_user].username) === 0){
+      readList = current_users[saved_user].readList;
+      for (pair in readList){
+        if (readList[pair].includes(title)){
+          return res.render(to_render, {error: "Book already in list"});
+        }
+      }
+      current_users[saved_user].readList.push([title, file]);
+      var added_users = JSON.stringify(current_users);
+      fs.writeFileSync('users.json', added_users);
+      return res.render(to_render, {error: ""});
+
+      }
+
+  }
+})
+
+app.get('/readlist', function(req, res){
+  user = req.session.user;
+  var current_users = fs.readFileSync('users.json');
+  current_users = JSON.parse(current_users);
+  for (saved_user in current_users){
+    if(user.localeCompare(current_users[saved_user].username) === 0){
+      var readList = current_users[saved_user].readList;
+      return res.render("readlist", {books: readList})
+    }
+  }
+})
+
+app.post('/search', function(req, res){
+   search  = req.body.Search.toLowerCase();
+   console.log(search);
+   var current_books = fs.readFileSync('books.json');
+   var searchbooks=[];
+   current_books = JSON.parse(current_books);
+   for(book in current_books){
+     if(current_books[book][0].toLowerCase().includes(search)){
+       searchbooks.push(current_books[book]);
+     }
+   }
+   if(searchbooks.length==0){
+     return res.render("searchresults",{error: "Book not found",books: searchbooks});
+   }
+   
+   return res.render("searchresults", {books: searchbooks})
+})
+
+
+
 
 app.listen(3000); //listens to all the requests to the local server
 
